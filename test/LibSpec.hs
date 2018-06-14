b647e786856a2b568013b967a3e79c54820562fd
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2018-06-14 11:57:55 +0200

    short tests

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -12,4 +12,17 @@ import Lib
 
 spec :: Spec
 spec = do
-    pure ()
+    describe "decToBin" $ do
+        it "never crashes" $ producesValidsOnValids decToBin
+        it "produces strings that only ever contain ones and zeroes" $
+            forAllValid $ \i ->
+                all (\c -> c == '0' || c == '1') $
+                dropWhile (== '-') $ decToBin i
+        it "deals with -1 correctly" $ decToBin (-1) `shouldBe` "-1"
+        it "produces positive numbers for positive integers" $
+            forAll (abs <$> genValid) $ \i ->
+                case decToBin i of
+                    [] -> False
+                    ('-':_) -> False
+                    _ -> True
+        it "deals with 59 correctly" $ decToBin 59 `shouldBe` "111011"
