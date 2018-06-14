cfdb97ae037963738e87b7bdbe2d6e8ded7be1c8
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2018-06-14 11:35:39 +0200

    second step

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Lib where
 
@@ -12,9 +13,39 @@ main = pure ()
 data HourMinute = HourMinute
     { hours :: Integer
     , minutes :: Integer
-    } deriving (Generic, Show, Eq)
+    , seconds :: Integer
+    , picoseconds :: Integer
+    } deriving (Generic, Show, Eq, Ord)
 
-instance Validity HourMinute
+instance Validity HourMinute where
+    validate HourMinute {..} =
+        mconcat
+            [ delve "hours" hours
+            , delve "minutes" minutes
+            , delve "picoseconds" picoseconds
+            , declare "Hours are positive" $ hours >= 0
+            , declare "hours are fewer than 24" $ hours < 24
+            , declare "minutes are positive" $ minutes >= 0
+            , declare "minutes are fewer than 60" $ minutes < 60
+            , declare "seconds are positive" $ seconds >= 0
+            , declare "seconds are fewer than 1e12" $ seconds < 60
+            , declare "picoseconds are positive" $ picoseconds >= 0
+            , declare "picoseconds are fewer than 1e12" $
+              picoseconds < (10 ^ 12)
+            ]
 
-hourMinute :: NominalDiffTime -> HourMinute
-hourMinute = undefined
+hourMinute :: Integer -> HourMinute
+hourMinute x =
+    let totalHours = totalMinutes `div` 60
+        totalMinutes = totalSeconds `div` 60
+        totalSeconds = totalPicoseconds `div` (10 ^ 12)
+        totalPicoseconds = x
+        hours = totalHours
+        minutes = totalMinutes - 60 * hours
+        seconds = totalSeconds - 60 * minutes
+        picoseconds = totalPicoseconds - (10 ^ 12) * seconds
+    in HourMinute {..}
+
+toPicoseconds :: HourMinute -> Integer
+toPicoseconds HourMinute {..} =
+    ((((hours * 60) + minutes) * 60) + seconds) * (10 ^ 12) + picoseconds
--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeApplications #-}
+
 module LibSpec where
 
 import Test.Hspec
@@ -8,8 +10,20 @@ import Data.GenValidity.Time ()
 
 import Lib
 
+instance GenUnchecked HourMinute
+
+instance GenValid HourMinute
+
 spec :: Spec
 spec = do
     describe "hourMinute" $
-        it "produces valid HourMinutes for valid NominalDiffTimes" $
+        it "produces valid HourMinutes for valid Integers" $
         producesValidsOnValids hourMinute
+    describe "toNominalDiffTime" $ do
+        it "is the inverse of hourMinute if you start with an Integer" $
+            inverseFunctionsOnValid hourMinute toPicoseconds
+        it "is the inverse of hourMinute if you start with a HourMinute" $
+            inverseFunctionsOnValid toPicoseconds hourMinute
+    genValidSpec @HourMinute
+    eqSpec @HourMinute
+    ordSpec @HourMinute
