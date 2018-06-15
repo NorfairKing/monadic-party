c62ae12de64f50ec677c67a75b5b0f0dc6d57357
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2018-06-15 11:24:28 +0200

    golden tests done

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,26 +6,41 @@ module Lib where
 
 import GHC.Generics (Generic)
 
-import Data.Validity
+import Control.Applicative
 import Data.Aeson as JSON
+import Data.Validity
 
 main :: IO ()
 main = pure ()
 
 data Recurrence
-    = EveryDay
-    | EveryMonth
+    = EveryDay Int
+    | EveryMonth Int
     deriving (Show, Eq, Generic)
 
 instance Validity Recurrence
 
 instance ToJSON Recurrence where
-    toJSON EveryDay = JSON.String "every-day"
-    toJSON EveryMonth = JSON.String "every-month"
+    toJSON (EveryDay i) = object ["every-day" .= i]
+    toJSON (EveryMonth i) = object ["every-month" .= i]
 
+-- object :: [Pair] -> Value
+-- (.=) :: (KeyValue kv, ToJSON v) => Text -> v -> kv
+-- instance KeyValue Pair
 instance FromJSON Recurrence where
-    parseJSON =
-        JSON.withText "Recurrence" $ \t ->
-            case t of
-                "every-day" -> pure EveryDay
-                "every-month" -> pure EveryMonth
+    parseJSON v =
+        JSON.withText
+            "Recurrence"
+            (\t ->
+                 case t of
+                     "every-day" -> pure (EveryDay 1)
+                     "every-month" -> pure (EveryMonth 1))
+            v <|>
+        JSON.withObject "Recurrence" (\o -> EveryDay <$> o .: "every-day") v<|>
+
+        JSON.withObject "Recurrence" (\o -> EveryMonth <$> o .: "every-month") v
+-- parseJSON :: JSON.Value -> Parser Recurrence
+-- withText :: String -> (Text -> Parser a) -> Value -> Parser a
+-- withObject :: String -> (Object -> Parser a) -> Value -> Parser a
+-- (<|>) :: Parser a -> Parser a -> Parser a
+-- (.:) :: FromJSON a => Object -> Text -> Parser a
--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -17,16 +17,21 @@ spec :: Spec
 spec = do
     describe "JSON Recurrence" $ do
         it "should encode EveryDay like this" $
-            JSON.encode EveryDay `shouldBe` "\"every-day\""
+            JSON.encode (EveryDay 2) `shouldBe` "{\"every-day\":2}"
+        it "should read EveryDay like this (golden)" $
+            JSON.decode "\"every-day\"" `shouldBe` Just (EveryDay 1)
         it "should read EveryDay like this" $
-            JSON.decode "\"every-day\"" `shouldBe` Just EveryDay
+            JSON.decode "{\"every-day\": 6}" `shouldBe` Just (EveryDay 6)
         it "should encode EveryMonth like this" $
-            JSON.encode EveryMonth `shouldBe` "\"every-month\""
+            JSON.encode (EveryMonth 7) `shouldBe` "{\"every-month\":7}"
+        it "should read EveryMonth like this (golden)" $
+            JSON.decode "\"every-month\"" `shouldBe` Just (EveryMonth 1)
         it "should read EveryMonth like this" $
-            JSON.decode "\"every-month\"" `shouldBe` Just EveryMonth
+            JSON.decode "{ \"every-month\": 5}" `shouldBe` Just (EveryMonth 5)
         it "should roundtrip" $
             forAllValid $ \r ->
                 (decode (encode r) :: Maybe Recurrence) `shouldBe` (Just r)
 
 instance GenUnchecked Recurrence
+
 instance GenValid Recurrence
