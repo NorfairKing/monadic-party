ff1a00e98d58d98595964e6d7aa9cdf5b6b25624
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2018-06-15 09:56:49 +0200

    day 3

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,27 +3,18 @@
 
 module Lib where
 
-import Data.Maybe
-import Data.Time
-import Data.Validity
 import GHC.Generics (Generic)
 
-readsInt :: String -> Maybe Integer
-readsInt = extract . reads
-  where
-    extract [(n, "")] = Just n
-    extract _ = Nothing
+import Data.Aeson as JSON
 
-decToBin :: Integer -> String
-decToBin num
-    | abs num < 2 = show num
-    | otherwise = decToBin (half num) ++ (show $ mod num 2)
-  where
-    half n = absIntHalf n * signum n
-    absIntHalf = floor . (/ 2) . fromIntegral . abs
+main :: IO ()
+main = pure ()
 
-main = do
-    putStrLn "Enter decimal value:"
-    val <- getLine
-    putStrLn . fromMaybe "Invalid Input" . fmap decToBin . readsInt $ val
-    main
+data Recurrence
+    = EveryDay
+    | EveryMonth
+    deriving (Show, Eq, Generic)
+
+instance ToJSON Recurrence
+
+instance FromJSON Recurrence
--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module LibSpec where
 
@@ -8,21 +9,18 @@ import Test.Validity
 
 import Data.GenValidity.Time ()
 
+import Data.Aeson as JSON
+
 import Lib
 
 spec :: Spec
 spec = do
-    describe "decToBin" $ do
-        it "never crashes" $ producesValidsOnValids decToBin
-        it "produces strings that only ever contain ones and zeroes" $
-            forAllValid $ \i ->
-                all (\c -> c == '0' || c == '1') $
-                dropWhile (== '-') $ decToBin i
-        it "deals with -1 correctly" $ decToBin (-1) `shouldBe` "-1"
-        it "produces positive numbers for positive integers" $
-            forAll (abs <$> genValid) $ \i ->
-                case decToBin i of
-                    [] -> False
-                    ('-':_) -> False
-                    _ -> True
-        it "deals with 59 correctly" $ decToBin 59 `shouldBe` "111011"
+    describe "JSON Recurrence" $ do
+        it "should encode EveryDay like this" $
+            JSON.encode EveryDay `shouldBe` "\"every-day\""
+        it "should read EveryDay like this" $
+            JSON.decode "\"every-day\"" `shouldBe` Just EveryDay
+        it "should encode EveryMonth like this" $
+            JSON.encode EveryMonth `shouldBe` "\"every-month\""
+        it "should read EveryMonth like this" $
+            JSON.decode "\"every-month\"" `shouldBe` Just EveryMonth
